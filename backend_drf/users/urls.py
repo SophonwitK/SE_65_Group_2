083defbcdcb88fc940d4965a1dc539722a76c443
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-03-05 23:39:19 +0700

    -

--- backend_drf/users/urls.py
+++ backend_drf/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     #path("register",views.register,name="register"),
+     path("register",views.register,name="register"),
+     path("login",views.login,name="login"),
+     path("user",views.user,name="user"),
+     path("logout",views.logout,name="logout"),
 ]
