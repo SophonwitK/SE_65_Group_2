7fd0c13ebb5b18a426b6ef47601fc7267d526cac
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-03-07 15:15:36 +0700

    validate
    email , password ,username

--- backend_drf/users/serializers.py
+++ backend_drf/users/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import User
+import re
+from django.core.validators import EmailValidator
+from django.db import IntegrityError
+from django.core.validators import RegexValidator
 
 class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(validators=[EmailValidator])
+    username = serializers.CharField(
+        validators=[
+            RegexValidator(
+                regex=r'^[A-Za-z0-9@#$%^&+-=]+$',
+                message='Username can only contain English letters (both uppercase and lowercase), numbers, or special characters',
+                code='invalid_username'
+            )
+        ]
+    )
+
     class Meta:
         model = User
         fields = [
@@ -17,8 +32,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
+        pattern = r'^[A-Za-z0-9@#$%^&+-=]+$'
+        if not re.match(pattern, password):
+            raise serializers.ValidationError("Password must contain only English letters (both uppercase and lowercase) or numbers or special characters.")
+        
         if password is not None:
             instance.set_password(password)
         instance.save()
         return instance
-        
