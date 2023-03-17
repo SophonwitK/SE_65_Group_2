eaf72681f825b4cbb6d6bf208105aa3f2087377d
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-03-17 22:07:07 +0700

    remove comment
    remove comment from table donateaccept

--- givepaws.sql
+++ givepaws.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 17, 2023 at 04:01 PM
+-- Generation Time: Mar 17, 2023 at 04:05 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -409,7 +409,6 @@ CREATE TABLE `donateaccept` (
   `hcID` int(20) NOT NULL,
   `date` datetime NOT NULL,
   `description` varchar(10000) NOT NULL,
-  `comment` varchar(1000) DEFAULT NULL,
   `cardID` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
