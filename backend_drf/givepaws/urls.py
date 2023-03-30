14db9067a9412616da2f304109f198ee81160be1
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-03-30 21:22:29 +0700

    Update urls.py

--- backend_drf/givepaws/urls.py
+++ backend_drf/givepaws/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 urlpatterns = [
     path("hospitals/",views.hospital_list,name="hospital_list"),
     path("hospitals/<int:pk>",views.hospital_detail,name="hospital_detail"),
-    path("users/",views.user_list,name="hospital_list"),
-    path("users/<int:pk>",views.user_detail,name="hospital_detail"),
+    path("users/",views.user_list,name="user_list"),
+    path("users/<int:pk>",views.user_detail,name="user_detail"),
     path("authens/",views.authen_list,name="authen_list"),
     path("authens/<int:pk>",views.authen_detail,name="authen_detail"),
     path("authenimages/",views.authenimages_list,name="authenimages_list"),
